85003153a9fbc1679992abf8a35c57aa380bb12f
yanniboi <yan@yanniboi.com>
2015-02-14 14:47:11 +0000

    initial commit

---
+++ js/flow.js
@@ -0,0 +1,30 @@
+$( document ).ready(function () {
+
+    // Function to hide show panels.
+    var showText = function (showClass, hideClass) {
+        $('.'+hideClass).hide();
+        jQuery('.'+showClass).slideDown(800);
+    };
+
+    $('button').click(function () {
+
+        // Change image src
+        $('img').attr('src', 'http://placehold.it/700x150');
+
+
+        // Get class from button
+        var BtnClass = $(this).attr('class');
+
+
+        if (BtnClass == 'left'){
+            showText('panel-left','panel-right');
+
+        }
+        else {
+            showText('panel-right','panel-left');
+
+        }
+
+    });
+});
+
